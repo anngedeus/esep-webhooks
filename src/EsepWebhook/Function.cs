ff1a7c3e6778b89b6180757744c762241502931f
anngedeus <76603380+anngedeus@users.noreply.github.com>
2023-04-02 16:38:46 -0400

    changed entire function

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Text;
-using Newtonsoft.Json;
-
 using Amazon.Lambda.Core;
-
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
